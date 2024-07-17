9409d7c3d1f0e5e2aab26b00c44ff7b4f1bfb863
egasa21 <esaputra168@gmail.com>
2024-07-17 20:55:01 +0700

    feat: project setup

---
+++ src/app.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const logger = require('morgan')('dev')
+const dotenv = require('dotenv')
+const ClientError = require('./exception/ClientError');
+const db = require('./models');
+
+dotenv.config()
+const app = express()
+
+app.use(logger);
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
+const syncDB = async () => {
+    try {
+        await db.sequelize.sync()
+        console.log('Database synced successfully')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// syncDB()
+
+
+app.get('/', (req, res) => {
+    res.json("/hello")
+})
+
+// Custom error handling
+app.use((err, req, res, next) => {
+    if (err instanceof ClientError) {
+        return res.status(err.statusCode).json({ error: err.message });
+    }
+
+    // For unexpected errors
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+});
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`)
+})
---
+++ src/config/database.js
@@ -0,0 +1,21 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+module.exports = {
+  development: {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: 26257,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  },
+  production: {
+    dialect: 'postgres',
+    host: 'your_production_host',
+    port: 26257,
+    username: 'your_production_username',
+    password: 'your_production_password',
+    database: 'your_production_database',
+  },
+};
